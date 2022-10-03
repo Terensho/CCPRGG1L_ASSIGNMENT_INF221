1901f2c0ed938a3ae5b08ae29eda3ea55caf5424
Dionela, Terrence A <111472732+Terensho@users.noreply.github.com>
2022-10-03 11:30:42 +0800

    Create App.java

---
+++ Assignment 2/App.java
@@ -0,0 +1,33 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        for (int i = 5; i != 0; i--) {
+           
+        //BREAK
+        if (i == 4){
+            System.out.println("Taympers");
+            break;
+        }
+         System.out.println("spanish bread");
+        int rideCount = 5;
+        do {
+            System.out.println("Dinasor");
+            rideCount--;
+        } while (rideCount != 0);
+
+            int nickname = 5;
+
+        while (nickname != 0) {
+            System.out.println("Dragon");
+            nickname--;
+            if (nickname == 3){
+                continue;
+            }
+        }
+    }
+
+  }
+
+    
+}
+    
